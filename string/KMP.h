5a440e9a616c33147ddc293679f496620cb4f5bf
bingxin-ly <lbx163_224360@163.com>
2023-01-17 21:08:02 +0800

    add KMP and prefix-f

--- string/KMP.h
+++ string/KMP.h
@@ -1,11 +1,13 @@
+// 前缀函数与 KMP 算法: https://oi-wiki.org/string/kmp/
 #include <vector>
 #include <string>
+using std::string;
 using std::vector;
 
 /**
  * @deprecated Native implementation, O(n^3)
  */
-vector<int> prefix_fun(std::string &s)
+vector<int> prefix_fun_(string &s)
 {
     int sz = s.size();
     vector<int> pi(sz);
@@ -22,7 +24,7 @@ vector<int> prefix_fun(std::string &s)
 /**
  * @deprecated Native implementation, O(n^2)
  */
-vector<int> prefix_fun(std::string &s)
+vector<int> prefix_fun__(string &s)
 {
     int sz = s.size();
     vector<int> pi(sz);
@@ -34,4 +36,40 @@ vector<int> prefix_fun(std::string &s)
                 break;
             }
     return pi;
+}
+
+/**
+ * @brief Searching for the pi substring. Last implementation
+ */
+vector<int> prefix_fun(string &s)
+{
+    int n = s.size();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++)
+    {
+        int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j - 1];
+        if (s[i] == s[j])
+            j++; // found
+        pi[i] = j;
+    }
+    return pi;
+}
+
+/**
+ * @brief KMP: Searching for the substrings
+ */
+vector<int> find_occurences(string text, string pattern)
+{
+    string cur = pattern + "#" + text;
+    int n = text.size(), m = pattern.size();
+    vector<int> v;
+    vector<int> lps = prefix_fun(cur);
+    for (int i = m + 1; i <= n + m; i++)
+    {
+        if (lps[i] == m)
+            v.push_back(i - 2 * m);
+    }
+    return v;
 }

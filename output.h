04d6d28512f0ed51565d2e38a02cdb26ab5816a2
bingxin-ly <lbx163_224360@163.com>
2023-01-18 11:31:37 +0800

    update

--- output.h
+++ output.h
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <initializer_list>
-#define r(i, n, k) for (int i = n; i < k; i++) //'<' inside instead of '<='
+//'<' inside instead of '<='
+#define r(i, n, k) for (int i = n; i < k; i++)
+using std::clog;
+
 void output(std::initializer_list<int> il, bool endl = true)
 {
     for (auto i : il)
-        std::cout << i << ' ';
+        clog << i << ' ';
     if (endl)
-        std::cout << std::endl;
+        clog << std::endl;
+}
+
+template <class T>
+inline void log(const T &x, const char c = '\n')
+{
+    std::clog << x << c;
+}
+template <class T, class... Args>
+inline void log(const T &x, const Args &...args)
+{
+    log(x, ' '), log(args...);
 }
